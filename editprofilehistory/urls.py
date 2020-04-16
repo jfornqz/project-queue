69b814527d932b2b2fa4e8543665308e859d8aad
everestes <42941606+everestes@users.noreply.github.com>
2020-04-16 23:06:26 +0700

    เพิ่มรูป / แก้ regis_med / แก้ urls views editpro /เพิ่ม editprofile html / แก้ base
    เพิ่มรูป / แก้ regis_med / แก้ urls views editpro /เพิ่ม editprofile html / แก้ base

--- editprofilehistory/urls.py
+++ editprofilehistory/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     
-    
+      path('editprofile/',  views.editprofile, name='editprofile'),
 ]
--- editprofilehistory/views.py
+++ editprofilehistory/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def editprofile(request):
+    context = {}
+    return render(request, 'editprofilehistory/editprofile.html', context)
