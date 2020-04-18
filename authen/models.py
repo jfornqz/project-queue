c48d60b11ef615ee4efd57e50ba0a6a4cb04865c
l3luehunter <l3luehunter@gmail.com>
2020-04-18 21:42:44 +0700

    Migrate

--- authen/models.py
+++ authen/models.py
@@ -3,14 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Admin(models.Model):
-    account_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) 
+    account_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True) 
 
 class Patient(models.Model):
-    pt_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) 
     age = models.IntegerField(max_length=3)
-    date_of_birth = models.DateField(auto_now=False)
-    phone = models.CharField(max_length=255) 
+    phone = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    name_title = models.CharField(max_length=255)
+    dob = models.DateField(auto_now=False)
+    address = models.TextField()
+    account_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
 
 class Medical_Personal(models.Model):
-    me_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True) 
     position = models.CharField(max_length=255)
+    account_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
