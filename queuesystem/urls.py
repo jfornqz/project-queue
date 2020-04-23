f8732ba1efa9cc37f75a33b3981b0cdd5490afe1
everestes <42941606+everestes@users.noreply.github.com>
2020-04-24 02:04:38 +0700

    update การนัด , url view queue system / base
    appointment / url view ใน queuesystem / base html

--- queuesystem/urls.py
+++ queuesystem/urls.py
@@ -4,12 +4,14 @@ from . import views
 urlpatterns = [
     
     path('', views.index, name='index'),
-    path('appointment_create/',  views.appointment_create, name='appointment_create'),
+    path('appointment_create/<int:num>/',  views.appointment_create, name='appointment_create'),
     path('appoitment_check/',  views.appoitment_check, name='appoitment_check'),
-    path('main_appoitment/',  views.main_appoitment, name='main_appoitment'),
+    path('search_appointment/',  views.search_appointment, name='search_appointment'),
+    path('main_appointment/',  views.main_appointment, name='main_appointment'),
     path('generate_queue/',  views.generate_queue, name='generate_queue'),
     path('remaining_queue/',  views.remaining_queue, name='remaining_queue'),
     path('run_queue/',  views.run_queue, name='run_queue'),
     path('main_patient/',  views.main_patient, name='main_patient'),
     path('main_medicalpersonnel/',  views.main_medicalpersonnel, name='main_medicalpersonnel'),
+
 ]
--- queuesystem/views.py
+++ queuesystem/views.py
@@ -1,5 +1,13 @@
 
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.forms import formset_factory
+from django import forms
+from authen.models import Medical_Personal, Patient
+from datetime import date, datetime
+
+
 
 # Create your views here.
 def index(request):
@@ -30,12 +38,40 @@ def appoitment_check(request):
     context = {}
     return render(request, 'queuesystem/appoitmentcheck.html', context)
 
-def appointment_create(request):
-    context = {}
+def appointment_create(request, num):
+    user = User.objects.get(pk=num)
+    patient = Patient.objects.get(account_id_id=num)
+    context = {
+                'user': user,
+                'patient': patient
+            }
     return render(request, 'queuesystem/appointmentcreate.html', context)
 
-def main_appoitment(request):
-    context = {}
+def search_appointment(request):
+    search = request.POST.get('search', '')
+    first_name_patient = User.objects.filter(first_name__icontains=search)
+    id_patient = User.objects.filter(id__icontains=search)
+    result = zip(id_patient, first_name_patient)
+    # if id_patient.exists():
+    #    result = zip(id_patient, first_name_patient)
+    context = {
+        'firstname_patient' : first_name_patient,
+        'id_patient' : id_patient,
+        'result' : result,
+        'search' : search,
+
+
+    }
+    return render(request, 'queuesystem/searchappointment.html', context)
+
+
+def main_appointment(request):
+    user = User.objects.get(pk=num)
+    patient = Patient.objects.get(account_id_id=num)
+    context = {
+                'user': user,
+                'patient': patient
+            }
     return render(request, 'queuesystem/mainappointment.html', context)
 
 
