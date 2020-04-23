9334334a2e8437053f5697a04301b11fb3484656
everestes <42941606+everestes@users.noreply.github.com>
2020-04-24 01:02:12 +0700

    แก้ไป 18 ไฟล์
    - template ใน user profile ทั้งหมด
    - template ใน queuesystem ทั้งหมด
    -url view ทั้ง user profile และ queuesystem
    -base.html
    - static > css > main.css , bootstap.css

--- queuesystem/urls.py
+++ queuesystem/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('appointment_create/',  views.appointment_create, name='appointment_create'),
+    path('appoitment_check/',  views.appoitment_check, name='appoitment_check'),
+    path('main_appoitment/',  views.main_appoitment, name='main_appoitment'),
+    path('generate_queue/',  views.generate_queue, name='generate_queue'),
+    path('remaining_queue/',  views.remaining_queue, name='remaining_queue'),
+    path('run_queue/',  views.run_queue, name='run_queue'),
+    path('main_patient/',  views.main_patient, name='main_patient'),
+    path('main_medicalpersonnel/',  views.main_medicalpersonnel, name='main_medicalpersonnel'),
 ]
--- queuesystem/views.py
+++ queuesystem/views.py
@@ -6,3 +6,36 @@ def index(request):
     context = {}
     return render(request, 'queuesystem/index.html', context)
 
+def main_patient(request):
+    context = {}
+    return render(request, 'queuesystem/main_patient.html', context)
+
+def main_medicalpersonnel(request):
+    context = {}
+    return render(request, 'queuesystem/main_medicalpersonnel.html', context)
+
+def run_queue(request):
+    context = {}
+    return render(request, 'queuesystem/runqueue.html', context)
+
+def remaining_queue(request):
+    context = {}
+    return render(request, 'queuesystem/remainingqueue.html', context)
+
+def generate_queue(request):
+    context = {}
+    return render(request, 'queuesystem/generatequeue.html', context)
+
+def appoitment_check(request):
+    context = {}
+    return render(request, 'queuesystem/appoitmentcheck.html', context)
+
+def appointment_create(request):
+    context = {}
+    return render(request, 'queuesystem/appointmentcreate.html', context)
+
+def main_appoitment(request):
+    context = {}
+    return render(request, 'queuesystem/mainappointment.html', context)
+
+
