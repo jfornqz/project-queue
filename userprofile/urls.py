2c93801194d82f17fc6b0ea4c6ad6526a477d248
jfornqz <42958376+jfornqz@users.noreply.github.com>
2020-04-25 18:45:03 +0700

    search

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django_filters.views import FilterView
+
+
 
 urlpatterns = [
       path('editprofile/<int:num>/',  views.editprofile, name='editprofile'),
-      path('search/',  views.search, name='search'),
+      path('search/', views.search, name='search'),
       path('add_admittedpatienthistory/<int:num>/',  views.admittedpatienthistory, name='admittedpatienthistory'),
       path('search_admitted/',  views.search_foradmitted, name='search_foradmitted'),
 ]
